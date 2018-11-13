d6758150c4b45092362165fc738e2c052db26ca7
wildenali <wildeeeeen@gmail.com>
2018-11-13 21:48:42 +0700

    GUI sederhana just helo

---
+++ 01_helo_1/helloGUI.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# Membuat GUI sederhana
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+
+if __name__== '__main__':
+    a = QApplication(sys.argv)
+
+    form = QWidget()
+    form.resize(300,200)
+    form.move(300,300)
+    form.setWindowTitle('GUI Sederhana')
+
+    label = QLabel('Hayy hay...')
+    label.move(55,40)
+    label.setParent(form)
+
+    form.show()
+
+    a.exec()
