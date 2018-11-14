287d7b554a7a039bb8ded7cfddaa054b8dece0d0
wildenali <wildeeeeen@gmail.com>
2018-11-14 23:02:13 +0700

    horizontal layout

---
+++ 09_HorizontalLayout/mainform.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QHBoxLayout
+
+class MainForm(QWidget):
+    def __init__(self):
+        super(MainForm, self).__init__()
+        self.setupUI()
+
+    def setupUI(self):
+        self.resize(300,200)
+        self.move(300,300)
+        self.setWindowTitle('QVerticalLayout')
+
+        self.button1 = QPushButton('Batagor')
+        self.button2 = QPushButton('Siomay')
+        self.button3 = QPushButton('Martabak')
+        self.button4 = QPushButton('Cilok')
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.button3)
+        layout.addWidget(self.button4)
+
+        self.setLayout(layout)
