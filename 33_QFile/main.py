9ff53bb3972f1a917bb69c85447d794ad46f3fa1
wildenali <wildeeeeen@gmail.com>
2018-11-20 11:51:21 +0700

    membuat dan mengakses file

--- 33_QFile/main.py
+++ 33_QFile/main.py
@@ -11,9 +11,60 @@ class MainForm(QWidget):
         self.setupUI()
 
     def setupUI(self):
-        self.resize(800,400)
+        self.resize(500,300)
         self.move(300,100)
-        self.setWindowTitle('Table Widget')
+        self.setWindowTitle('Akses File')
+
+        self.label1 = QLabel('Nama File')
+        self.fileEdit = QLineEdit()
+        self.label2 = QLabel('Data yang akan di TULIS')
+        self.inputTextEdit = QTextEdit()
+        self.label3 = QLabel('Data yang di BACA')
+        self.outputTextEdit = QTextEdit()
+        self.outputTextEdit.setReadOnly(True)
+
+        self.writeButton = QPushButton('Tulis Data')
+        self.readButton = QPushButton('Baca Data')
+        self.readButton.setDisabled(True)
+        hbox= QHBoxLayout()
+        hbox.addStretch()
+        hbox.addWidget(self.writeButton)
+        hbox.addWidget(self.readButton)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label1)
+        layout.addWidget(self.fileEdit)
+        layout.addWidget(self.label2)
+        layout.addWidget(self.inputTextEdit)
+        layout.addWidget(self.label3)
+        layout.addWidget(self.outputTextEdit)
+        layout.addLayout(hbox)
+        self.setLayout(layout)
+
+        self.writeButton.clicked.connect(self.writeButtonClick)
+        self.readButton.clicked.connect(self.readButtonClick)
+
+    def writeButtonClick(self):
+        if len(self.fileEdit.text().strip()) == 0:
+            QMessageBox.critical(self, 'Kesalahan', 'Nama file harus diisi')
+            return
+
+        if self.inputTextEdit.document().isEmpty():
+            QMessageBox.critical(self, 'Kesalahan', 'Data yang akan di tulis harus diisi')
+            return
+
+        f = open(self.fileEdit.text(), 'w')
+        f.writelines(self.inputTextEdit.document().toPlainText())
+        f.close()
+        self.fileEdit.setDisabled(True)
+        self.inputTextEdit.setDisabled(True)
+        self.writeButton.setDisabled(True)
+        self.readButton.setDisabled(False)
+
+    def readButtonClick(self):
+        f = open(self.fileEdit.text(), 'r')
+        self.outputTextEdit.document().setPlainText(''.join(f.readlines()))
+        f.close()
 
 
 if __name__ == '__main__':
